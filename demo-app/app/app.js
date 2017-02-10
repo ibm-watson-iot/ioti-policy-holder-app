d2d6e42d012c14861c4c8248be1437573933019f
Fatih Ulusoy <ulusoy@de.ibm.com>
2017-02-10 16:36:55 +0100

    Seperated config files for dev and production.

--- demo-app/app/app.js
+++ demo-app/app/app.js
@@ -90,7 +90,4 @@ angular.module('BlurAdmin', [
         }
       }
     });
-})
-.constant('apiProtocol', 'https')
-.constant('apiHost', 'iot4insurance-api-393b0bf3-830a-4065-a906-50ef7fa967e4.eu-gb.mybluemix.net')
-.constant('apiPath', '/');
+});
---
+++ demo-app/app/config-staging.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('BlurAdmin').constant('apiProtocol', 'https');
+angular.module('BlurAdmin').constant('apiHost', '');
+angular.module('BlurAdmin').constant('apiPath', '/');
