1a8eadd38653c14a46eb7e55adce36fca3406b9f
Dragos Cojocari <dragos.cojocari@ro.ibm.com>
2017-09-26 10:43:33 +0300

    Correct example file

--- app/config-staging.js
+++ app/config-staging.js
@@ -2,12 +2,12 @@
 
 angular.module('BlurAdmin.configs', []);
 
-angular.module('BlurAdmin.configs').constant('apiProtocol', 'http');
-angular.module('BlurAdmin.configs').constant('apiHost', '173.193.106.78:31005');
+angular.module('BlurAdmin.configs').constant('apiProtocol', 'https');
+angular.module('BlurAdmin.configs').constant('apiHost', 'ioti.us-south.containers.mybluemix.net');
 angular.module('BlurAdmin.configs').constant('apiPath', '/api/v1');
-angular.module('BlurAdmin.configs').constant('tenantId', '5239b7869beb835582b0fc399a21bae9');
+angular.module('BlurAdmin.configs').constant('tenantId', '<tenantid>');
 
 angular.module('BlurAdmin.configs').constant('backendProtocol', 'https');
-angular.module('BlurAdmin.configs').constant('backendHost', 'iot4i-starter-backend.mybluemix.net');
+angular.module('BlurAdmin.configs').constant('backendHost', '<backendhost>:<port>');
 angular.module('BlurAdmin.configs').constant('backendPath', '/api/v1');
 angular.module('BlurAdmin.configs').constant('backendWebSocketPath', '/notifications');
