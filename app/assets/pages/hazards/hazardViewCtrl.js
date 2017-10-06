996d02a11c7ea37dc073536706c904ee219fd75f
Bilal Al-Saeedi <bilal.alsaeedi@de.ibm.com>
2017-10-06 15:50:42 +0200

    Correctly show hazards locations

--- app/assets/pages/hazards/hazardViewCtrl.js
+++ app/assets/pages/hazards/hazardViewCtrl.js
@@ -1,80 +1,45 @@
 (function() {
-'use strict';
+  'use strict';
 
-angular.module('BlurAdmin.pages.hazards').controller('HazardViewCtrl', HazardViewCtrl);
+  angular.module('BlurAdmin.pages.hazards').controller('HazardViewCtrl', HazardViewCtrl);
 
-function HazardViewCtrl($stateParams, $filter, toastr, hazardService, shieldService, userService) {
-  var vm = this;
-  vm.hazard = {};
+  function HazardViewCtrl($stateParams, $filter, toastr, hazardService, shieldService, gmapsHandler) {
+    const vm = this;
+    vm.hazard = {};
 
-  if ($stateParams.hazardEventId) {
-    hazardService.find($stateParams.hazardEventId).success(function(hazard) {
-      vm.hazard = hazard;
-
-      initializeLocationMap(hazard.userId);
-
-      shieldService.find(vm.hazard.shieldId).success(function(shield) {
-        vm.shield = shield;
+    if ($stateParams.hazardEventId) {
+      hazardService.find($stateParams.hazardEventId).success((hazard) => {
+        vm.hazard = hazard;
+        showInMap(hazard);
+        shieldService.find(vm.hazard.shieldId).success((shield) => {
+          vm.shield = shield;
+        });
       });
-    });
-  }
+    }
 
-  vm.acknowledgeHazard = function(hazard) {
-    hazard.ishandled = true;
-    hazardService.updatePartial(hazard._id, {ishandled: true}).success(function(data) {
-      toastr.success("Acknowledged.");
-    }).error(function(err) {
-      toastr.error("Saving hazard has failed!", "Error");
-    });
-  };
-
-  function initializeLocationMap(userId) {
-    // get user location
-    userService.find(userId).then(function(resp) {
-      var user = resp.data;
-      var address = user.address.street + ", " + user.address.zipcode + " " + user.address.city + ", " + user.address.country;
-      var geocoder;
-      var map;
-
-      geocoder = new google.maps.Geocoder();
-      var latlng = new google.maps.LatLng(-34.397, 150.644);
-      var myOptions = {
-        zoom: 14,
-        center: latlng,
-        mapTypeControl: true,
-        mapTypeControlOptions: { style: google.maps.MapTypeControlStyle.DROPDOWN_MENU },
-        navigationControl: true,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      map = new google.maps.Map(document.getElementById("google-maps"), myOptions);
-      if (geocoder) {
-        geocoder.geocode({ 'address': address }, function (results, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
-            if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
-              map.setCenter(results[0].geometry.location);
-              var infowindow = new google.maps.InfoWindow(
-                {
-                  content: '<b>' + address + '</b>',
-                  size: new google.maps.Size(150, 50)
-                });
-              var marker = new google.maps.Marker({
-                position: results[0].geometry.location,
-                map: map,
-                title: address
-              });
-              google.maps.event.addListener(marker, 'click', function () {
-                infowindow.open(map, marker);
-              });
-            } else {
-              console.log("No results found");
-            }
-          } else {
-            console.log("Geocode was not successful for the following reason: " + status);
+    const showInMap = (hazard) => {
+      gmapsHandler.initGmaps();
+      if (hazard.locations) {
+        hazard.locations.forEach((location) => {
+          if (location.geometry && location.geometry.coordinates) {
+            gmapsHandler.showInMap({
+              type: 'latLng',
+              latLng: {
+                lat: location.geometry.coordinates[0],
+                lng: location.geometry.coordinates[1]
+              }
+            });
           }
         });
       }
-    });
+    };
+
+    vm.acknowledgeHazard = (hazard) => {
+      hazard.ishandled = true;
+      hazardService.updatePartial(hazard._id, { ishandled: true }).success((data) => {
+        toastr.success("Acknowledged.");
+      }).error(err => toastr.error("Saving hazard has failed!", "Error"));
+    };
   }
-}
 
 })();
